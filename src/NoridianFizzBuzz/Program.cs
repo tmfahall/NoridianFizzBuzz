3abd54ad0e4fc42eb955b1b48b5423f54e0abe6d
Andrew Hall <tmfahall@gmail.com>
2016-04-01 00:01:09 -0500

    uploaded files that didn't initially upload

---
+++ src/NoridianFizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NoridianFizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; ++i)
+            {
+                if (i % 14 == 0)
+                {
+                    Console.WriteLine("CrossShield");
+                }
+
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine("Shield");
+                }
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Cross");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
